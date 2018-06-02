5381427830d336163f51f831d9f6f505ebcc5326
mallano <maelallano1@gmail.com>
2018-06-02 17:55:18 +0200

    data.js updated

---
+++ public/asset/js/data.js
@@ -0,0 +1,37 @@
+const tl1 = [
+	{
+		situation: "Wesh gros, présente toi",
+		answers: [
+      		answer1 = {
+        		content: "Suivant",
+        		target: [0, 1]
+      		}
+    	]
+	},
+	{
+		situation: "Je suis Bob, j'ai 22ans, 3 femmes 2 enfants, et je voudrais aller en :",
+		answers: [
+      		answer1 = {
+        		content: "bachelor 3D",
+        		target: [1, 0]
+      		},
+      		answer2 = {
+        		content: "bachelor Web",
+        		target: [0, 2]
+      		},
+      		answer3 = {
+        		content: "grande école",
+        		target: [1, 1]
+      		}
+    	]
+	}
+]
+
+const tl2 = [
+	{
+    	situation: "Vous êtes mort psk 3D"
+   	},
+	{
+    	situation: "Vous êtes mort psk grande école"
+   	}
+]
