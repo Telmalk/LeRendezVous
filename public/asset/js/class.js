4365a82cc03392ac0f217a9099ae5c64fd17a715
Clément Haller <clement.haller@hetic.net>
2018-06-02 16:36:24 +0200

    architecture

---
+++ public/asset/js/class.js
@@ -0,0 +1,19 @@
+class Player {
+    constructor();
+}
+
+
+
+class View {
+    dekstop() {
+        var template = `
+            <div></div>
+                
+        `
+    }
+
+    island() {
+        body.innerHtml
+    }
+}
+
