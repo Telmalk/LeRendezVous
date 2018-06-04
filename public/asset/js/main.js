6de655fade6b2b1925d5bac32f4e4ea6eeb0a5bc
Clément Haller <clement.haller@hetic.net>
2018-06-04 13:24:33 +0200

    generation automatoqie de l algo

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -1,32 +1,63 @@
 let timeLine = [tl1, tl2];
 
-const templateCore = () => {
+const templateCore = (situation, choices) => {
+    document.querySelector("body").innerHTML = "";
+    let myDiv = document.createElement('div');
+    let inteCore = `<section class="core">
+       <div class="container">
+           <img class="imgSituation" src="./asset/img/desert-island.png">
+           <div class="characterContainer">
+               <img class="characterImg" src="./asset/img/brontis.jpg">
+           </div>
+           <div class="bubbleText">
+               <p class="text situation">${situation}</p>
+               <div class="answerContainer">
+                  <!-- <p class="text answer">bachelor 3D</p>
+                   <p class="text answer">bachelor Web</p>
+                   <p class="text answer">grande école</p> -->
+               </div>
+               <div class="arrow-left"></div>
+           </div>
+       </div>
+    </section>`;
 
+    myDiv.innerHTML = inteCore;
+    document.querySelector('body').appendChild(myDiv);
+    let answerContainer = document.querySelector(".answerContainer");
+    for (let i = 0; i < choices.length; i++) {
+        let myAnswer = document.createElement('p');
+        myAnswer.dataset.target = choices[i].target;
+        myAnswer.innerHTML = choices[i].content;
+        myAnswer.classList.add('text', 'answer');
+        answerContainer.appendChild(myAnswer);
+    }
+    console.log(myDiv);
+    return myDiv;
 };
 
-
-
-const display = (text, choices) => {
+const display = (situation, choices) => {
     let body = document.querySelector('body');
-    let template = `
-        <p>${text}</p>
-    `;
-    let templateChoice = ``;
-    for (let i = 0; i < choices.length; i++) {
-        templateChoice += `<div data-target="${choices[i].target}" class="next">${choices[i].content}</div>`
+    let newChoice = templateCore(situation, choices);
+    body.appendChild(newChoice);
+    let but = document.querySelectorAll(".answer");
+    for (let i = 0; i < but.length; i++) {
+        but[i].addEventListener("click", function () {
+            console.log('toto');
+            let target = this.getAttribute("data-target");
+            let redirection = target.split(',');
+            display(timeLine[redirection[0]][redirection[1]].situation, timeLine[redirection[0]][redirection[1]].answers);
+        });
     }
-    body.innerHTML = template + templateChoice;
-    let but = document.querySelectorAll(".next");
-        for (let i = 0; i < but.length; i++) {
-            but[i].addEventListener("click", function () {
-                let target = this.getAttribute("data-target");
-                let wesh = target.split(',');
-                console.log(wesh);
-                display(timeLine[wesh[0]][wesh[1]].situation, timeLine[wesh[0]][wesh[1]].answers);
-
-            });
-        }
 };
 
 
 display(timeLine[0][0].situation, timeLine[0][0].answers);
+
+/*let template = `
+    <p>${situation}</p>
+`;*/
+//let templateChoice = ``;
+//for (let i = 0; i < choices.length; i++) {
+//templateChoice += `<div data-target="${choices[i].target}" class="next">${choices[i].content}</div>`
+//}
+//document.querySelector('body').innerHTML = templateCore();
