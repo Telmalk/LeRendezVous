55cc56c95f9fb52d02dc72dd31fd8df3526d9c9c
Clément Haller <clement.haller@hetic.net>
2018-06-07 01:58:55 +0200

    jsais plus

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -1,4 +1,4 @@
-let timeLine = [tl1, tl2];
+let timeLine = [brontisOfficeTL, endsTL, islandTL];
 
 const imageFade = (fade) => {
     let visible = 0.0;
@@ -7,16 +7,44 @@ const imageFade = (fade) => {
           fade.style.opacity = visible;
           visible += 0.1;
           visible = Math.round(10 * visible) / 10;
-          console.log(visible);
-      } else{
+      } else
           clearInterval(intervalID);
-      }
   }, 30);
+};
+
+const transiText = (text, target, answerContainer, choices, tabAnswer) => {
+    let textDelayed = "";
+    let i = 0;
+    let interval = setInterval(() => {
+     textDelayed += text[i];
+     target.textContent = textDelayed;
+     i++;
+     if (i >= text.length) {
+         for (i = 0; i < tabAnswer.length; i++) {
+             tabAnswer[i].dataset.target = choices[i].target;
+             tabAnswer[i].innerHTML = choices[i].content;
+         }
+         clearInterval(interval);
+     }
+    }, 5)
+};
+
+const setAudio = (audio) => {
+    if (audio !== false) {
+        let aud = document.createElement("audio");
+        console.log(aud);
+        aud.src = audio;
+        console.log(aud);
+        aud.play();
+        return aud;
+    } else
+        return false;
 };
 
 const templateCore = (situation, choices, character, background, audio) => {
     document.querySelector("body").innerHTML = "";
     let myDiv = document.createElement('div');
+    let tabAnswer = [];
      myDiv.innerHTML = `
         <section class="core">
             <div class="container">
@@ -25,7 +53,7 @@ const templateCore = (situation, choices, character, background, audio) => {
                     <img class="characterImg" src="${character}">
                 </div>
                 <div class="bubbleText">
-                   <p class="text situation">${situation}</p>
+                   <p class="text situation"></p>
                    <div class="answerContainer">
                    </div>
                    <div class="arrow-left"></div>
@@ -36,27 +64,30 @@ const templateCore = (situation, choices, character, background, audio) => {
     let answerContainer = document.querySelector(".answerContainer");
     for (let i = 0; i < choices.length; i++) {
         let myAnswer = document.createElement('p');
-        myAnswer.dataset.target = choices[i].target;
-        myAnswer.innerHTML = choices[i].content;
         myAnswer.classList.add('text', 'answer');
-        answerContainer.appendChild(myAnswer);
+        tabAnswer.push(myAnswer);
     }
-    if (audio !== false) {
-        let aud = document.createElement("audio");
-        aud.src = audio;
-        aud.play();
-        console.log(aud);
-        myDiv.appendChild(aud);
+    transiText(situation, document.querySelector('.situation'), answerContainer, choices, tabAnswer);
+    for (let i = 0; i < tabAnswer.length; i++) {
+        answerContainer.appendChild(tabAnswer[i]);
     }
+    let audioSet = setAudio(audio);
+    if (audioSet !== false)
+        myDiv.appendChild(audioSet);
     return myDiv;
 };
 
-const display = (situation, choices, character, background, audio = false) => {
+const display = (situation, choices, character, background, audio = false, redirection = 0) => {
     let body = document.querySelector('body');
+
     let newChoice = templateCore(situation, choices, character, background, audio);
     body.appendChild(newChoice);
-    let fade = document.querySelector(".imgSituation");
-    imageFade(fade);
+    console.log(redirection[0]);
+    if (redirection[0] != 1) {
+        let fade = document.querySelector(".imgSituation");
+        imageFade(fade);
+    } else
+        console.log("toto");
     let but = document.querySelectorAll(".answer");
     for (let i = 0; i < but.length; i++) {
         but[i].addEventListener("click", function() {
@@ -66,9 +97,12 @@ const display = (situation, choices, character, background, audio = false) => {
                 timeLine[redirection[0]][redirection[1]].answers,
                 timeLine[redirection[0]][redirection[1]].character,
                 timeLine[redirection[0]][redirection[1]].background,
-                timeLine[redirection[0]][redirection[1]].audio);
+                timeLine[redirection[0]][redirection[1]].audio,
+                redirection);
         });
     }
 };
 
-display(timeLine[0][0].situation, timeLine[0][0].answers, timeLine[0][0].character, timeLine[0][0].background);
+display(timeLine[0][0].situation, timeLine[0][0].answers,
+    timeLine[0][0].character, timeLine[0][0].background,
+    );
