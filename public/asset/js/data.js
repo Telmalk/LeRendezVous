c75aa30b4479bc92feacdcde7ef99ce9d37db8a8
mallano <maelallano1@gmail.com>
2018-06-07 09:41:03 +0200

    scneario updated

--- public/asset/js/data.js
+++ public/asset/js/data.js
@@ -52,9 +52,13 @@ const brontisOfficeTL = [
               target: [2, 0]
           },
           answer2 = {
+              content: "Je n'ai besoin d'aucun objets pour survivre moi ! OKLM",
+              target: [1, 10]
+          },
+          /*answer2 = {
               content: "Une machette, un briquet et le collier légué par ma grand mère.",
               target: [2, 0]
-          },
+          },*/
           answer3 = {
               content: "Un pistolet, du rhum et une fusée de detresse.",
               target: [3, 0]
@@ -67,6 +71,29 @@ const brontisOfficeTL = [
         character: "./asset/img/brontis.jpg",
         background: "./asset/img/desert-island.png",
         answers: []
+  },
+  {
+        situation: "Vous rentrez dans le bureau de Brontis pour continuer l'entretien.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/desert-island.png",
+        answers: [
+          answer1 = {
+              content: "Suivant",
+              target: [0, 6]
+          }
+        ]
+  },
+  {
+      //brontis
+        situation: "Félicitation ! Vous m'impressionez par votre capacité d'adaptation ! Cependant, vous sentez l'alcool à plein nez et c'est innaxceptable de votre part.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/desert-island.png",
+        answers: [
+          answer1 = {
+              content: "C pa ce ke vou kroyé mon bont monçieur",
+              target: [1, 9]
+          }
+        ]
   }
 ];
 
@@ -160,6 +187,94 @@ const endsTL = [
             answer2 = {
                 content: "Recommencer au début de l'île",
                 target: [2, 0]
+            }
+        ],
+    },
+    {
+      //Grontis 6
+        situation: "Selon les termes du pari, et l'ayant perdu, Grontis fait sortir votre âme de votre corps. Vous êtes mort.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/desert-island.png",
+        answers: [
+            answer1 = {
+                content: "Recommencer",
+                target: [0, 0]
+            },
+            answer2 = {
+                content: "Recommencer au début de l'île",
+                target: [3, 0]
+            }
+        ],
+    },
+    {
+      //Bateau 7
+        situation: "Le bateau réplique avec ses canons. Et malheureusement pour vous, il vise bien. Vous êtes mort.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/desert-island.png",
+        answers: [
+            answer1 = {
+                content: "Recommencer",
+                target: [0, 0]
+            },
+            answer2 = {
+                content: "Recommencer au début de l'île",
+                target: [3, 0]
+            }
+        ],
+    },
+    {
+      //Jack Sparrow 8
+        situation: "Vous pensiez vraiment pouvoir être plus rapide que le CAPITAINE Jack Sparrow ?",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/desert-island.png",
+        answers: [
+            answer1 = {
+                content: "Recommencer",
+                target: [0, 0]
+            },
+            answer2 = {
+                content: "Recommencer au début de l'île",
+                target: [3, 0]
+            }
+        ],
+    },
+    {
+      //Jack Sparrow 8
+        situation: "L'homme vous tire dessus et repart tranquillement sur son bateau. Oui, vous êtes bien mort.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/desert-island.png",
+        answers: [
+            answer1 = {
+                content: "Recommencer",
+                target: [0, 0]
+            },
+            answer2 = {
+                content: "Recommencer au début de l'île",
+                target: [3, 0]
+            }
+        ],
+    },
+    {
+      //Yann 9
+        situation: "Un homme ressemblant comme deux gouttes d'eau à Agrid entre dans le bureau et vous jette en dehors d'HETIC d'un mouvement d'auriculaire. Comme vous l'avez compris, vous n'êtes pas admis.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/desert-island.png",
+        answers: [
+            answer1 = {
+                content: "Recommencer",
+                target: [0, 0]
+            }
+        ],
+    },
+    {
+      //Island without objects 10
+        situation: "Un flash de lumière rempli la salle, vous ouvrez alors vos yeux et découvrez, ébahie, que vous êtes en plein milieu d'une île déserte. Cependant, vous croyiez vraiment pouvoir survivre sans objets ? Vous errez pendant 42 jours sur l'île puis mourrez.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/desert-island.png",
+        answers: [
+            answer1 = {
+                content: "Recommencer",
+                target: [0, 0]
             }
         ],
     }
@@ -344,8 +459,206 @@ const islandJackSparrowTL = [
         answers: [
             answer1 = {
                 content: "Suivant",
-                target: [2, 1]
+                target: [3, 1]
             }
         ],
-    }
+    },
+    {
+        situation: "À 1km sur votre droite vous pouvez apercevoir la silhouette d'un homme. Sur votre gauche vous apercevez une plage au loin.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/desert-island.png",
+        answers: [
+            answer1 = {
+                content: "Allez à droite vers l'homme.",
+                target: [3, 2]
+            },
+            answer2 = {
+                content: "Allez à gauche vers la plage.",
+                target: [3, 11]
+            }
+        ],
+    },
+    {
+        situation: "Vous arrivez à l'endroit où vous aviez aperçu la silhouette de l'homme mais pas moyen de le trouver ... Vous apercevez alors une lampe posée sur le sol.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Vous décidez de la frotter ...",
+                target: [3, 3]
+            },
+            answer1 = {
+                content: "Ça semble trop évident que c'est un piège ... Retourner vers la plage.",
+                target: [3, 11]
+            }
+        ],
+    },
+    {
+        situation: "Beaucoup de fumée sort alors de cette lampe puis se condense et prend forme humaine.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Suivant",
+                target: [3, 4]
+            }
+        ],
+    },
+    {
+        situation: "Vous semblez connaitre cette personne ... Vous vous souvenez soudainement et vous exclamez: \"Brontis ? C'est vous ?!\"",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Suivant",
+                target: [3, 5]
+            }
+        ],
+    },
+    {
+        situation: "Sacré Bob, tu dois sûrement me confondre avec mon cousin germain ! Moi, je suis le génie Grontis. Je vais te proposer un défi, si tu le réussis je réaliserai un de tes voeux, sinon, tu mourra. Qu'en dis-tu ?",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "J'accepte !",
+                target: [3, 6]
+            },
+            answer2 = {
+                content: "Non merci, mourrir ne me tente pas trop. Je vais plutôt retourner vers la plage au loin.",
+                target: [3, 11]
+            }
+        ],
+    },
+    {
+        situation: "Très bien, le défi est simple, tu dois simplement gagner au pierre, feuille, ciseau contre moi. C'est parti !",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Suivant",
+                target: [3, 7]
+            }
+        ],
+    },
+    {
+        situation: "Perre ... feuille ... ciseau !",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Pierre !",
+                target: [3, 8]
+            },
+            answer2 = {
+                content: "Feuille !",
+                target: [3, 9]
+            },
+            answer3 = {
+                content: "Ciseau !",
+                target: [3, 10]
+            }
+        ],
+    },
+    {
+        situation: "Feuille ! Tu as perdu. Je peux te laisser un autre chance si tu veux ?",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Oui",
+                target: [3, 7]
+            },
+            answer2 = {
+                content: "Non",
+                target: [1, 6]
+            }
+        ],
+    },
+    {
+        situation: "Ciseau ! Tu as perdu. Je peux te laisser un autre chance si tu veux ?",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Oui",
+                target: [3, 7]
+            },
+            answer2 = {
+                content: "Non",
+                target: [1, 6]
+            }
+        ],
+    },
+    {
+        situation: "Pierre ! Tu as perdu. Je peux te laisser un autre chance si tu veux ?",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Oui",
+                target: [3, 7]
+            },
+            answer2 = {
+                content: "Non",
+                target: [1, 6]
+            }
+        ],
+    },
+    {
+        situation: "Vous arrivez sur la plage et voyez un bateau au loin.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Lui tirer dessus avec votre pistolet.",
+                target: [1, 7]
+            },
+            answer2 = {
+                content: "Tirer votre fusée de détresse.",
+                target: [3, 12]
+            }
+        ],
+    },
+    {
+        situation: "Un homme sort du bateau et rejoint la plage sur une barque. Il vous pointe avec un pistolet et s'exclame: \"Hey l'ami, j'espère pour toi que tu as de l'or à me donner.\".",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Lui tirer dessus avec votre pistolet.",
+                target: [1, 8]
+            },
+            answer2 = {
+                content: "Lui proposer du rhum",
+                target: [3, 13]
+            },
+            answer2 = {
+                content: "Être sincère et lui dire que non.",
+                target: [1, 9]
+            }
+        ],
+    },
+    {
+        situation: "J'aime quand on me parle comme ça l'ami ! Allons festoyer sur mon navire !",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Suivant.",
+                target: [3, 14]
+            }
+        ],
+    },
+    {
+        situation: "Après 3 jours et quelques comas éthyliques vous lui demander de vous ramener à HETIC, ce qu'il accepte.",
+        character: "./asset/img/brontis.jpg",
+        background: "./asset/img/denis.jpg",
+        answers: [
+            answer1 = {
+                content: "Suivant.",
+                target: [0, 5]
+            }
+        ],
+    },
 ]
